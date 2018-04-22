aa928ef1b770c4b9a39da60196f66f8d73174cc9
niracler <1026037967@qq.com>
2018-04-22 11:15:10 +0800

    限制了长度为800,并且改变了数据库的连接方式

--- scraper/spiders/spider.py
+++ scraper/spiders/spider.py
@@ -58,8 +58,8 @@ class Spider(scrapy.Spider):
         if not content:
             return
 
-        if len(content) > 1800:
-            content = content[:1800] + "..."
+        if len(content) > 800:
+            content = content[:800] + "..."
 
         item = ArticleItem()
         item['table_name'] = str(talbe_name)
